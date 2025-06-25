1426821acc52d7c6a592f11f0e85b7bbdba86057
Damien Lecointre <damien.lecointre75@gmail.com>
2025-06-25 11:55:53 +0200

    update connect properties to allow more time to fetch all data

--- models/connection.js
+++ models/connection.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 15000,
+  })
   .then(() => console.log("Database connected"))
   .catch((error) => console.error(error));
